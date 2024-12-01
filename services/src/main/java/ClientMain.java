4ebfc5eff69e8fc63651751bea6f66711fd0ecf5
Alex <alex.bah2022@yandex.ru>
2024-12-01 03:11:00 +0300

    initial commit

---
+++ services/src/main/java/ClientMain.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class ClientMain {
+    public static void main(String[] args) {
+        JavaChatClient javaChatClient = new JavaChatClient();
+        javaChatClient.connect();
+//        javaChatClient.exit();
+    }
+}
